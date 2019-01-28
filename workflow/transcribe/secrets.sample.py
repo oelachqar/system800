a7ae544905381ee6fcfe54fcc369e3582515c151
oelachqar <elachqaroussama@gmail.com>
2019-01-27 21:42:43 -0800

    remove obsolete file

--- workflow/transcribe/secrets.sample.py
+++
@@ -1,3 +0,0 @@
-bing_speech_api_key="bing speech api key"
-google_credentials_json = '{"project_id": "..."}'
-google_preferred_phrases = ['list of phrases', ] # https://cloud.google.com/speech/docs/basics#phrase-hints
