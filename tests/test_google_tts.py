346a7a6e6a310328a798980e568878fc64d8d813
oelachqar <elachqaroussama@gmail.com>
2019-01-27 21:39:51 -0800

    black style formatting

--- tests/test_google_tts.py
+++ tests/test_google_tts.py
@@ -7,13 +7,13 @@ from workflow.transcribe.google_tts import GoogleTranscriber
 class TestGoogleTranscriber(unittest.TestCase):
     def setUp(self):
         self.google_transcriber = GoogleTranscriber(
-            TestConfig.google_credentials_json, google_preferred_phrases=None)
+            TestConfig.google_credentials_json, google_preferred_phrases=None
+        )
         self.audio_path = TestConfig.test_google_audio_path
         self.expected_text = "you are acting so weird and immature"
 
     def test_transcribe(self):
-        transcript = self.google_transcriber.transcribe_audio_file_path(
-            self.audio_path)
+        transcript = self.google_transcriber.transcribe_audio_file_path(self.audio_path)
         self.assertEqual(transcript[0].strip(), self.expected_text)
 
 
