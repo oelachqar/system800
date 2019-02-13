3afc80e2e7d67760865178386e485f82115cc2b0
oelachqar <elachqaroussama@gmail.com>
2019-02-12 20:07:17 -0800

    use post form for callback_url as well

--- api/app.py
+++ api/app.py
@@ -172,7 +172,7 @@ def process():
         response.status_code = 400
         return response
 
-    callback_url = request.args.get("callback_url")
+    callback_url = request.values.get("callback_url")
 
     # check callback url is here
     try:
